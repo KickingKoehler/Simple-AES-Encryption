1ff6535f5778121587de7148889d994c42022a0f
KoehlerKode <98114152+KoehlerKode@users.noreply.github.com>
2023-08-26 11:13:36 -0400

    Changed the name to Koder-AES-Encryption; Removed PyCharm files; Include more randomization
    Changed the name to Koder-AES-Encryption.
    Removed all the PyCharm project files.
    Points are now randomized.
    The number of iterations can now be determined easily by the user.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from Elliptic_Point import Elliptic_Point
 from Elliptic_Curve import Elliptic_Curve
 from AES_Encryptor import AES_Encryptor
 from AES_Decryptor import AES_Decryptor
+import random
 
 
 #Bob and Alice are two hypothetical people who are attempting to communicate with encrypted messages.
@@ -17,23 +18,34 @@ from AES_Decryptor import AES_Decryptor
 #Bob will use this shared secret key to encrypt a message. Finally, Alice will use the encrypted message and the shared secret key to produce the original message.
 
 def main():
+    answer = input("Do you want to enter a new number of iterations (default is 20000)? Y/N \n")
+    if answer == "y" or answer == "Y" or answer == "Yes" or answer == "yes":
+        times = int(input("How many times do you want the program to run? \n"))
+    elif answer == "n" or answer == "N" or answer == "No" or answer == "no":
+        times = 20000
+    else:
+        print('Please answer a valid input ("Yes", "No", "Y", "N").')
     curve = Elliptic_Curve()
     print(curve)
-    BobPoint = Elliptic_Point(curve,9860721)
+    randBob = random.randint(1000000,100000000)
+#    BobPoint = Elliptic_Point(curve,9860721)
+    BobPoint = Elliptic_Point(curve,randBob)
     print("Bob's point is: {0}.".format(BobPoint))
-    AlicePoint = Elliptic_Point(curve, 2813709)
+    randAlice = random.randint(1000000,100000000)
+#    AlicePoint = Elliptic_Point(curve, 2813709)
+    AlicePoint = Elliptic_Point(curve, randAlice)
     BobSharedSecretKey = BobPoint.keyGenerator(AlicePoint)
     print("Alice's point is: {0}.".format(AlicePoint))
     print("The Shared Secret which Bob has is: {0}".format((BobSharedSecretKey)))
     BobPlainText = '"It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of light, it was the season of darkness, it was the spring of hope, it was the winter of despair." -A Tale of Two Cities by Charles Dickens'
     BobEncryptor = AES_Encryptor(BobSharedSecretKey,BobPlainText)
-    BobCipherText = BobEncryptor.encrypt()
+    BobCipherText = BobEncryptor.encrypt(times)
     print("Bob's Ciphertext is:")
     print("{0}".format(BobCipherText[0]))
     AliceSharedSecretKey = AlicePoint.keyGenerator(BobPoint)
     print("The Shared Secret which Alice has is: {0}".format((AliceSharedSecretKey)))
     AliceDecryptor = AES_Decryptor(AliceSharedSecretKey,BobCipherText)
-    AlicePlainText = AliceDecryptor.decrypt()
+    AlicePlainText = AliceDecryptor.decrypt(times)
     print("Alice's PlainText is: {0}".format(AlicePlainText))
 
 if __name__ == '__main__':
